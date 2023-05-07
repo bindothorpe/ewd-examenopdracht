3192551411acddfaf989f22e76faf976a4aa45ac
bindothorpe <bindo.thorpe@student.hogent.be>
2023-05-07 19:57:46 +0200

    Added user to create controller

--- src/main/java/com/bindothorpe/ewd_examenopdracht/CreateController.java
+++ src/main/java/com/bindothorpe/ewd_examenopdracht/CreateController.java
@@ -1,21 +1,24 @@
 package com.bindothorpe.ewd_examenopdracht;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import repository.BookRepository;
+import service.UserService;
 
 @Controller
 @RequestMapping("/create")
 public class CreateController {
 
     @Autowired
-    private BookRepository bookRepository;
+    private UserService userService;
 
     @GetMapping
-    public String showView(Model model) {
+    public String showView(Model model, Authentication auth) {
+        model.addAttribute("user", userService.findByUsername(auth.getName()));
         return "create";
     }
 
--- src/main/java/com/bindothorpe/ewd_examenopdracht/SecurityConfig.java
+++ src/main/java/com/bindothorpe/ewd_examenopdracht/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder());
+
     }
 
 

38971420d062d74da636259a441ecfaeae1f0956
bindothorpe <bindo.thorpe@student.hogent.be>
2023-05-09 19:58:37 +0200

    Added message when adding/removing a book from your list

--- src/main/java/com/bindothorpe/ewd_examenopdracht/BooksController.java
+++ src/main/java/com/bindothorpe/ewd_examenopdracht/BooksController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import service.BookService;
 import service.UserService;
 
@@ -31,7 +32,7 @@ public class BooksController {
     }
 
     @GetMapping(value = "/{id}")
-    public String showBookOverview(@PathVariable("id") Long id, Model model, Authentication auth) {
+    public String showBookOverview(@PathVariable("id") Long id, Model model, Authentication auth, RedirectAttributes redirectAttributes) {
 
         Book book = bookService.findById(id);
 
@@ -43,19 +44,27 @@ public class BooksController {
         model.addAttribute("user", userService.findByUsername(auth.getName()));
         model.addAttribute("hasBook", book.getUsersList().contains(userService.findByUsername(auth.getName())));
 
+        String message = (String) redirectAttributes.getFlashAttributes().get("message");
+
+        if(message != null)
+            model.addAttribute("message", message);
+
         return "bookOverview";
     }
 
     @PostMapping(value = "/add/{id}")
-    public String addBookToUser(@PathVariable("id") Long id, Model model, Authentication auth) {
+    public String addBookToUser(@PathVariable("id") Long id, Model model, Authentication auth, RedirectAttributes redirectAttributes) {
         bookService.addUserToUsersList(id, userService.findByUsername(auth.getName()).getId());
+        redirectAttributes.addFlashAttribute("message", "Book added to your list");
         return "redirect:/books/" + id;
     }
 
 
     @PostMapping(value = "/remove/{id}")
-    public String removeBookFromUser(@PathVariable("id") Long id, Model model, Authentication auth) {
+    public String removeBookFromUser(@PathVariable("id") Long id, Model model, Authentication auth, RedirectAttributes redirectAttributes) {
         bookService.removeUserFromUsersList(id, userService.findByUsername(auth.getName()).getId());
+        redirectAttributes.addFlashAttribute("message", "Book removed from your list");
+
         return "redirect:/books/" + id;
     }
 
